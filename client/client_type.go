76623d99a9efee89ab973b1b0066f87e59377cee
Aleksey Suleymanov <aleksey.suleymanov@gmail.com>
2017-11-22 12:43:33 +0000

    Add File Please enter the commit message for your changes. Lines starting

---
+++ client/client_type.go
@@ -0,0 +1,15 @@
+package client
+
+type Beneficiarie struct {
+	Account string
+	Weight  uint16
+}
+
+type PC_Options struct {
+	Percent   uint16
+	BenefList []Beneficiarie
+}
+
+type PC_Vote struct {
+	Weight int
+}
