2d7a832db38c25d4f7960fd7efed9f711ba63d5e
Aleksey Suleymanov <aleksey.suleymanov@gmail.com>
2018-01-11 12:55:00 +0000

    Edit function

--- client/verify.go
+++ client/verify.go
@@ -117,27 +117,38 @@ func (api *Client) Verify_Post(author, permlink string) bool {
 	}
 }
 
-func (api *Client) Verify_Delegate_Posting_Key_Sign(username string, arr []string) []string {
-	var truearr []string
-
-	acc, err := api.Rpc.Database.GetAccounts(arr)
+func (api *Client) Verify_Delegate_Posting_Key_Sign(from_user, to_user string) bool {
+	acc, err := api.Rpc.Database.GetAccounts([]string{from_user})
 	if err != nil {
 		log.Println(errors.Wrapf(err, "Error Verify Delegate Vote Sign: "))
-		return nil
-	} else {
-		for _, val := range acc {
-			if val.Name == username {
-				truearr = append(truearr, val.Name)
-			} else {
-				for _, v := range val.Posting.AccountAuths {
-					l := strings.Split(strings.Replace(strings.Replace(fmt.Sprintf("%v", v), "[", "", -1), "]", "", -1), " ")[0]
-					if l == username {
-						truearr = append(truearr, val.Name)
-					}
+		return false
+	} else if len(acc) == 1 {
+		for _, v := range acc[0].Posting.AccountAuths {
+			tu := strings.Split(strings.Replace(strings.Replace(fmt.Sprintf("%v", v), "[", "", -1), "]", "", -1), " ")
+			if tu[0] == to_user {
+				if tu[1] == fmt.Sprintf("%v", acc[0].Posting.WeightThreshold) {
+					return true
 				}
 			}
 		}
+		return false
+	} else {
+		return false
 	}
+}
 
-	return truearr
+func (api *Client) Verify_First_Post(username string) bool {
+	d := time.Now()
+	cont, err := api.Rpc.Database.GetDiscussionsByAuthorBeforeDate(username, "", d.Format("2006-01-02T00:00:00"), 100)
+	if err != nil {
+		log.Println(errors.Wrapf(err, "Error Verify First Post: "))
+		return false
+	} else {
+		if len(cont) > 1 {
+			return false
+		} else {
+			return true
+		}
+		return false
+	}
 }
